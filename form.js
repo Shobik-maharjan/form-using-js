e717df1b33de5b8e4e35846e5836553df7e7eba6
Shobik-maharjan <shobitmah32@gmail.com>
2024-01-15 13:18:22 +0545

    done

--- form.js
+++ form.js
@@ -1,6 +1,12 @@
 // function submitBtn() {
 
 const form = document.getElementById("form");
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const number = document.getElementById("number");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+const email = document.getElementById("email");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -10,100 +16,116 @@ form.addEventListener("submit", (e) => {
 // }
 
 const validateInputs = () => {
-  const fname = document.getElementById("fname");
-  const lname = document.getElementById("lname");
-  const email = document.getElementById("email");
-  const number = document.getElementById("number");
-  const password = document.getElementById("password");
-  const password2 = document.getElementById("password2");
+  isValid = true;
+  firstName();
+  lastName();
+  emailCheck();
+  numberCheck();
+  passwordCheck();
 
-  if (fname.value === "" || fname.value === null) {
-    setError(fname, "first name is required");
-    setErrorColor("fname");
-    return false;
-  } else {
-    setSuccessColor("fname");
-    document.getElementById("fname").style.borderColor = "green";
-    setSuccess(fname);
-  }
-  if (lname.value === "" || lname.value === null) {
-    setError(lname, "last name is required");
-    setErrorColor("lname");
-    return false;
-  } else {
-    setSuccess(lname);
-    setSuccessColor("lname");
-  }
-  if (email.value === "" || email.value === null) {
-    setError(email, "email is required");
-    setErrorColor("email");
-    return false;
-  } else {
-    setSuccess(email);
-    setSuccessColor("email");
-  }
-  if (number.value === "" || number.value === null) {
-    setError(number, "number is required");
-    setErrorColor("number");
-    return false;
-  }
-  if (!number.value.match(numberFormat)) {
-    setError(number, "number must be 10 digits and starts from 98");
-    setErrorColor("number");
-    return false;
-  } else {
-    setSuccess(number);
-    setSuccessColor("number");
-  }
-
-  if (password.value === "" || password.value === null) {
-    setError(password, "password is required");
-    setErrorColor("password");
-    return false;
-  } else {
-    setSuccess(password);
-  }
-
-  if (!password.value.match(passwordFormat)) {
-    setError(
-      password2,
-      "The password must be more than 8 characters, less than 16 characters, 1 upper case, 1 numeric character, and one special character."
-    );
-    setErrorColor("password2");
-    return false;
-  }
-  if (password.value != password2.value) {
-    setError(password2, "password doesnot match");
-    setErrorColor("password2");
-    return false;
-  }
-  if (password.value == password2.value) {
-    setSuccess(password);
-    setSuccess(password2);
-    setSuccessColor("password");
-    setSuccessColor("password2");
+  if (isValid) {
     console.log(fname.value);
     console.log(lname.value);
     console.log(email.value);
+    console.log(number.value);
     console.log(password.value);
   }
 };
 
-const passwordFormat = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
-const numberFormat = /^98\d{8}$/;
+const firstName = () => {
+  if (fname.value === "" || fname.value === null) {
+    setError(fname, "first name is required");
+    setErrorColor("fname");
+    isValid = false;
+  } else {
+    setSuccessColor("fname");
+    setSuccess(fname);
+  }
+};
 
-// let confirmPassword = function () {
-//   if (
-//     document.getElementById("password").value ==
-//     document.getElementById("password2").value
-//   ) {
-//     document.getElementById("message").style.color = "green";
-//     document.getElementById("message").innerHTML = "Password match!";
-//   } else {
-//     document.getElementById("message").style.color = "red";
-//     document.getElementById("message").innerHTML = "Password do not match!";
-//   }
-// };
+const lastName = () => {
+  if (lname.value === "" || lname.value === null) {
+    setError(lname, "last name is required");
+    setErrorColor("lname");
+    isValid = false;
+  } else {
+    setSuccessColor("lname");
+    setSuccess(lname);
+  }
+};
+
+const emailCheck = () => {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  if (email.value === "" || email.value === null) {
+    setError(email, "email is required");
+    setErrorColor("email");
+    isValid = false;
+    // return false;
+  } else if (!email.value.match(emailRegex)) {
+    setError(email, "email format is not correct");
+    setErrorColor("email");
+    isValid = false;
+  } else {
+    setSuccess(email);
+    setSuccessColor("email");
+  }
+};
+
+const numberCheck = () => {
+  const numberRegex = /^98\d{8}$/;
+  if (number.value === "" || number.value === null) {
+    setError(number, "number is required");
+    setErrorColor("number");
+    isValid = false;
+    // return false;
+  }
+  if (!number.value.match(numberRegex)) {
+    setError(number, "number must be 10 digits and starts from 98");
+    setErrorColor("number");
+    isValid = false;
+    // return false;
+  } else {
+    setSuccess(number);
+    setSuccessColor("number");
+  }
+};
+
+const passwordCheck = () => {
+  const passwordRegex =
+    /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
+  if (password.value === "" || password.value === null) {
+    setError(password, "password is required");
+    setErrorColor("password");
+    isValid = false;
+    return false;
+  }
+  if (!password.value.match(passwordRegex)) {
+    setSuccess(password, "");
+    setError(
+      password2,
+      "The password must be more than 8 characters, less than 16 characters, 1 upper case, 1 numeric character, and one special character."
+    );
+    setErrorColor("password");
+    setErrorColor("password2");
+    isValid = false;
+    return false;
+  } else {
+    setSuccess(password);
+    setSuccessColor("password");
+  }
+
+  if (password.value != password2.value) {
+    setError(password2, "password doesnot match");
+    setErrorColor("password2");
+    isValid = false;
+    // return false;
+  } else {
+    setSuccess(password);
+    setSuccess(password2);
+    setSuccessColor("password");
+    setSuccessColor("password2");
+  }
+};
 
 const setError = (element, message) => {
   const inputControl = element.parentElement;
